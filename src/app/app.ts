3981752ab8279945c33d0554c522b41e6babb764
spataraia16 <a.pataraia16@gmail.com>
2026-03-22 21:12:51 +0400

    Draft2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Counter } from "./counter/counter";
 
+type Answer = {
+  answer: string,
+  correct: boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,31 @@ import { Counter } from "./counter/counter";
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
+
+
 export class App {
-  protected readonly title = signal('firstProject');
+  answer1: Answer = {answer: "Great!", correct: false};
+  answer2: Answer = {answer: "Fine", correct: true};
+  answer3: Answer = {answer: "Normal", correct: false};
+  answer4: Answer = {answer: "I wanna kms", correct: false};
+
+  constructor(){
+    let formEl = document.getElementById("formElement")
+
+    formEl?.addEventListener("click", function(event){
+      event.preventDefault();
+
+      function(e: Answer): void{
+        if (e.correct){
+          alert("success")
+        } else if(!e.correct){
+          alert("wrong")
+        } else {
+          alert("pick an answer first")
+        }
+      }
+    })
+  }
+  
 }
